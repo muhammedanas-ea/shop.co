c640335a86426c6f1b5ae986fab7e0725dbe096a
anasachu2000 <muhammedanasea2000@gmail.com>
2024-08-14 10:38:11 +0530

    update:hero section completed

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,10 +3,10 @@ import { Outlet } from "react-router-dom"
 
 const Layout = () =>{
     return(
-        <>
+        <div className="max-w-screen-2xl mx-auto">
         <Header/>
         <Outlet />
-        </>
+        </div>
     )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,48 +1,53 @@
 import heroImg from "../assets/heroImag/Rectangle_2-removebg-preview.png";
+import icon from "../assets/heroImag/Vector-removebg-preview.png"
 
 const HomePage = () => {
   return (
-    <section className="min-h-screen bg-[#F2F0F1] pt-5">
-      <div className="app-container flex flex-col lg:flex-row items-center lg:items-start relative">
-        {/* Text Section */}
-        <div className="flex-1 pt-8 lg:pt-14  lg:text-left px-4 lg:px-0">
-          <h1 className="font-bold text-3xl md:text-5xl lg:text-6xl max-w-lg mx-auto lg:mx-0">
-            FIND CLOTHES THAT MATCH YOUR STYLE
+    <section className="bg-[#F2F0F1] lg:h-screen">
+      <div className="app-container flex flex-col md:items-center lg:flex-row gap-5 ">
+        <div className="max-w-lg flex flex-col justify-center md:text-center lg:text-start mt-10 ">
+          <h1 className="font-bold text-4xl md:text-6xl">
+            FIND CLOTHES THAT MATCHES YOUR STYLE
           </h1>
-          <p className="max-w-lg text-sm md:text-base my-4 mx-auto lg:mx-0">
+          <p className="md:text-base text-sm mt-3 mb-5">
             Browse through our diverse range of meticulously crafted garments,
             designed to bring out your individuality and cater to your sense of
             style.
           </p>
-          <div className="flex justify-center lg:justify-start mt-4 lg:mt-8">
-            <button className="bg-black text-white py-4 px-8 rounded-[62px] w-full md:w-auto text-base">
-              Shop Now
-            </button>
-          </div>
-          {/* Stats Section */}
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-5 mt-8 mx-auto lg:mx-0 max-w-xs md:max-w-none">
+          <button className="w-full lg:w-52 rounded-[62px] py-4 bg-black text-white text-base">
+            Shop Now
+          </button>
+          <div className="mt-8 grid grid-cols-2 md:grid-cols-3 text-center gap-5">
             <div>
-              <h2 className="font-bold text-2xl md:text-4xl">200+</h2>
-              <p className="text-xs md:text-base">International Brands</p>
+              <h1 className="font-bold text-2xl md:text-4xl">200+</h1>
+              <h1 className="text-xs md:text-base text-[#0000008a]">
+                International Brands
+              </h1>
             </div>
             <div>
-              <h2 className="font-bold text-2xl md:text-4xl">2000+</h2>
-              <p className="text-xs md:text-base">High-Quality Products</p>
+              <h1 className="font-bold text-2xl md:text-4xl">2,000+</h1>
+              <h1 className="text-xs md:text-base text-[#0000008a]">
+                High-Quality Products
+              </h1>
             </div>
-            <div>
-              <h2 className="font-bold text-2xl md:text-4xl">30,000+</h2>
-              <p className="text-xs md:text-base">Happy Customers</p>
+            <div className="col-span-2 md:col-span-1">
+              <h1 className="font-bold text-2xl md:text-4xl">30,000+</h1>
+              <h1 className="text-xs md:text-base text-[#0000008a]">
+                Happy Customers
+              </h1>
             </div>
           </div>
         </div>
-
-        {/* Image Section */}
-        <div className="relative h-[30rem] w-96 md:h-[35rem] lg:h-auto mt-8 lg:mt-0 md:w-full lg:w-auto">
-          <img
-            className="w-full h-full object-cover lg:object-contain"
-            src={heroImg}
-            alt="Fashion showcase"
-          />
+        <div className="lg:mt-10 ">
+          <div className="lg:ml-24 relative">
+            <img src={heroImg} alt="" />
+            <div className="absolute top-0 w-16 h-16 md:w-28 md:h-28 right-0">
+              <img className="w-full h-full object-cover" src={icon} alt="" />
+            </div>
+            <div className="absolute top-36 md:w-14 md:h-14 w-11 h-11">
+              <img className="w-full h-full object-cover" src={icon} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </section>
