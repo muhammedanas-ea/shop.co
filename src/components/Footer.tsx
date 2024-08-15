7599cf11442a9864c2ad887aaefbe37d73b28d92
anasachu2000 <muhammedanasea2000@gmail.com>
2024-08-15 15:38:05 +0530

    update: footer part completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
             <div className="relative w-full">
               <FaEnvelope
                 size={20}
-                className="absolute left-6 top-7 transform -translate-y-1/2 text-gray-400"
+                className="absolute left-6 top-6 transform -translate-y-1/2 text-gray-400"
               />
               <input
                 className="rounded-[62px] text-base py-3 px-14 w-full border border-gray-300 focus:border-gray-500 focus:outline-none"
