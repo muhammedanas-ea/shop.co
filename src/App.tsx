b6b85431f7fe32b3e93359bdc3a7771972026e77
anasachu2000 <muhammedanasea2000@gmail.com>
2024-08-16 09:53:18 +0530

    update:routing bug

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserRouter />} />
+        <Route path="/*" element={<UserRouter />} />
       </Routes>
     </Router>
   );
