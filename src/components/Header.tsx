40aff48f4350bc09e7c9a73ffd4ccea35e830280
anasachu2000 <muhammedanasea2000@gmail.com>
2024-08-16 10:04:14 +0530

    update:routing bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { FiShoppingCart } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
 import { LuMenu } from "react-icons/lu";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <div className="app-container sticky top-0 bg-white z-30 border-b">
       <header className="py-5 flex items-center justify-between">
         <div className="flex items-center gap-8">
-          <LuMenu size={24} className="lg:hidden" />
-          <Link to={'/'}><h1 className="font-bold text-2xl md:text-3xl sub-font">SHOP.CO</h1></Link>
+          {isMobileMenuOpen ? (
+            <IoMdClose 
+              size={24}
+              className="lg:hidden cursor-pointer"
+              onClick={toggleMobileMenu}
+            />
+          ) : (
+            <LuMenu
+              size={24}
+              className="lg:hidden cursor-pointer"
+              onClick={toggleMobileMenu}
+            />
+          )}
+          <Link to={"/"}>
+            <h1 className="font-bold text-2xl md:text-3xl sub-font">SHOP.CO</h1>
+          </Link>
           <ul className="lg:flex items-center gap-5 hidden">
-            <Link to={'category'}><li className="font-normal text-base">Shop</li></Link>
+            <Link to={"/category"}>
+              <li className="font-normal text-base">Shop</li>
+            </Link>
             <li>On Sale</li>
             <li>New Arrivals</li>
             <li>Brands</li>
@@ -22,7 +46,22 @@ const Header = () => {
           <FaRegCircleUser size={24} />
         </div>
       </header>
+      {isMobileMenuOpen && (
+        <div className="lg:hidden bg-white shadow-md p-5  absolute top-full left-0 w-full z-20">
+          <ul className="flex flex-col gap-4">
+            <Link to={"/category"}>
+              <li onClick={toggleMobileMenu} className="font-normal text-base">
+                Shop
+              </li>
+            </Link>
+            <li onClick={toggleMobileMenu}>On Sale</li>
+            <li onClick={toggleMobileMenu}>New Arrivals</li>
+            <li onClick={toggleMobileMenu}>Brands</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
+
 export default Header;
