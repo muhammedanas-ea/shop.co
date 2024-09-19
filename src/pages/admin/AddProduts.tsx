9b7928b3efebd59948679c5a1c843320a2d1fea5
anasachu2000 <muhammedanasea2000@gmail.com>
2024-09-19 20:46:39 +0530

    update:product adding section completed

--- src/pages/admin/AddProduts.tsx
+++ src/pages/admin/AddProduts.tsx
@@ -5,7 +5,7 @@ const AddProducts = () => {
   return (
     <div className="p-5">
       {/* Header */}
-      <div className="flex gap-3 items-center mb-5 bg-gray-50 p-4 rounded-lg shadow-sm border border-gray-200">
+      <div className="flex gap-3 items-center mb-5 bg-white p-4 rounded-lg shadow-sm border border-gray-200">
         <Link to={"/admin/products"}>
           <button className="px-2 py-2 rounded-lg border">
             <IoIosArrowRoundBack size={24} />
@@ -17,6 +17,62 @@ const AddProducts = () => {
             Add New Product
           </h1>
         </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        <div className="bg-white p-4 rounded-lg border col-span-2">
+          <h1 className="font-medium text-lg text-gray-900 tracking-tight admin-font">
+            General information
+          </h1>
+          <div className="flex flex-col mt-5">
+            <label className="text-sm admin-font" htmlFor="product-name">
+              Name Product
+            </label>
+            <input
+              className="rounded-lg bg-gray-100 border-none mt-1"
+              type="text"
+              id="product-name"
+            />
+          </div>
+          <div className="flex flex-col mt-4">
+            <label className="text-sm admin-font" htmlFor="product-description">
+              Description Product
+            </label>
+            <textarea
+              className="rounded-lg bg-gray-100 border-none mt-1 h-32"
+              id="product-description"
+            ></textarea>
+          </div>
+          <div className="flex flex-col mt-4">
+            {/* Title Section */}
+            <p className="text-sm font-semibold text-gray-800">Size</p>
+            <p className="text-xs text-gray-500">Pick Available Size</p>
+
+            {/* Size Options */}
+            <div className="flex flex-row mt-3 space-x-3">
+              {/* Size Option 1 */}
+              <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-lg shadow-sm cursor-pointer hover:bg-green-300 active:bg-green-300 transition duration-150 ease-in-out">
+                <span className="text-sm">XS</span>
+              </div>
+              <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-lg shadow-sm cursor-pointer hover:bg-green-300 active:bg-green-300 transition duration-150 ease-in-out">
+                <span className="text-sm">S</span>
+              </div>
+
+              {/* Size Option 2 */}
+              <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-lg shadow-sm cursor-pointer hover:bg-green-300 active:bg-green-300 transition duration-150 ease-in-out">
+                <span className="text-sm ">M</span>
+              </div>
+
+              {/* Size Option 3 */}
+              <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-lg shadow-sm cursor-pointer hover:bg-green-300 active:bg-green-300 transition duration-150 ease-in-out">
+                <span className="text-sm ">XL</span>
+              </div>
+              <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-lg shadow-sm cursor-pointer hover:bg-green-300 active:bg-green-300 transition duration-150 ease-in-out">
+                <span className="text-sm ">XXL</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white h-1/2">2</div>
       </div>
     </div>
   );
--- src/pages/admin/Products.tsx
+++ src/pages/admin/Products.tsx
@@ -58,7 +58,7 @@ const Products = () => {
   return (
     <div className="p-5">
       {/* Header */}
-      <div className="flex justify-between items-center mb-5 bg-gray-50 p-4 rounded-lg shadow-sm border border-gray-200">
+      <div className="flex justify-between items-center mb-5 bg-white p-4 rounded-lg shadow-sm border border-gray-200">
         <div>
           <p className="text-sm text-gray-400 admin-font">
             Manage your inventory
@@ -69,9 +69,9 @@ const Products = () => {
         </div>
 
         <Link to={"/admin/addproducts"}>
-          <button className="bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-lg text-white flex items-center shadow-md gap-1 focus:outline-none focus:ring-4 focus:ring-teal-300">
-            <IoIosAdd size={24} />
-            <span className="admin-font">Add New Product</span>
+          <button className="bg-blue-500 text-base hover:bg-blue-600 px-4 py-2 rounded-lg text-white flex items-center shadow-md gap-1 focus:outline-none focus:ring-4 focus:ring-teal-300">
+            <IoIosAdd size={24} color="#ffff"/>
+            <span className="admin-font ">Add New Product</span>
           </button>
         </Link>
       </div>
@@ -94,8 +94,8 @@ const Products = () => {
               <h2 className="text-lg font-semibold text-gray-800 admin-font">
                 {product.name}
               </h2>
-              <p className="text-gray-600 mt-2 admin-font">{product.price}</p>
-              <div className="mt-4 flex justify-between items-center">
+              <p className="text-gray-600 mt-1 admin-font">{product.price}</p>
+              <div className="mt-3 flex justify-between items-center">
                 <button className="text-blue-600 hover:underline admin-font">
                   Edit
                 </button>
